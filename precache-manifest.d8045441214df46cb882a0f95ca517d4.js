6ee460b53412caf1a71b0bcc64e8e0eb87341f05
Falafel95 <70195376+Falafel95@users.noreply.github.com>
2020-09-14 10:45:49 +0200

    Updates

---
+++ precache-manifest.d8045441214df46cb882a0f95ca517d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e06e26c5f75e3cb6bededc8802f10bb",
+    "url": "/github.io-FaceFinder/index.html"
+  },
+  {
+    "revision": "7fe7e2be9943914f339d",
+    "url": "/github.io-FaceFinder/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d68c6d1da71f98709b22",
+    "url": "/github.io-FaceFinder/static/css/main.a1f37f4f.chunk.css"
+  },
+  {
+    "revision": "7fe7e2be9943914f339d",
+    "url": "/github.io-FaceFinder/static/js/2.f7f02fa2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/github.io-FaceFinder/static/js/2.f7f02fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68c6d1da71f98709b22",
+    "url": "/github.io-FaceFinder/static/js/main.22207bf3.chunk.js"
+  },
+  {
+    "revision": "ceb7fa36ac62d951f3ab",
+    "url": "/github.io-FaceFinder/static/js/runtime-main.083d1fb5.js"
+  }
+]);
